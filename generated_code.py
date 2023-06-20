7592589bf6dac5e8120bbab0507765a00ee25045
Nikolaos Konstantinou <77862574+nccon@users.noreply.github.com>
2023-06-20 21:47:16 +0300

    Initial commit

---
+++ generated_code.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Project Manager v3</title>
+    <style>
+      body {
+        background: linear-gradient(darkblue, lightblue);
+        font-family: sans-serif;
+      }
+    </style>
+  </head>
+  <body>
+    <h1>Project Manager v3</h1>
+    <p>
